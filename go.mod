50add6cdd29f0120620ede8eaea2c54edb843bcb
Tim Steffens <t.steffens@tarent.de>
2020-09-18 15:05:22 +0200

    use kafka-testing-go v0.1.5

--- go.mod
+++ go.mod
@@ -4,11 +4,9 @@ go 1.13
 
 require (
 	github.com/confluentinc/confluent-kafka-go v1.4.2
-	github.com/docker/go-connections v0.4.0
 	github.com/fvosberg/errtypes v0.0.0-20190318091206-e95b29c1c67c
-	github.com/google/uuid v1.1.1
 	github.com/segmentio/kafka-go v0.4.2
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.6.1
-	github.com/testcontainers/testcontainers-go v0.7.0
+	github.com/tmstff/kafka-testing-go v0.1.5
 )
--- topics_test.go
+++ topics_test.go
@@ -2,18 +2,12 @@ package kafkaadmin
 
 import (
 	"context"
-	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
-	"github.com/testcontainers/testcontainers-go"
-	"log"
+	"github.com/tmstff/kafka-testing-go"
 	"math/rand"
-	"net"
-	"os"
 	"strconv"
-	"strings"
 	"testing"
 	"time"
 )
@@ -22,7 +16,7 @@ func TestTopicCreation(t *testing.T) {
 
 	ctx := context.Background()
 
-	kafkaUrl, terminateKafka, err := StartKafkaWithEnv(ctx, map[string]string{"KAFKA_AUTO_CREATE_TOPICS_ENABLE": "false"})
+	kafkaUrl, terminateKafka, err := kafkatesting.StartKafkaWithEnv(ctx, map[string]string{"KAFKA_AUTO_CREATE_TOPICS_ENABLE": "true"})
 	if err != nil {
 		assert.Fail(t, err.Error())
 	} else {
@@ -66,147 +60,4 @@ func TestTopicCreation(t *testing.T) {
 	// ensure it still works if topic is present
 	err = EnsureTopicExistsWithConfig(ctx, kafkaUrl, nil, config)
 	assert.Nil(t, err)
-}
-
-type TestLogConsumer struct {
-	containerName string
-}
-
-func (g *TestLogConsumer) Accept(l testcontainers.Log) {
-	logrus.Infof("%s: %s", g.containerName, string(l.Content))
-}
-
-// deprecated
-func StartKafkaContainer(ctx context.Context) (kafkaUrl string, terminateFunction func(context.Context) error, err error) {
-	return StartKafkaWithEnv(ctx, map[string]string{})
-}
-
-func StartKafka(ctx context.Context) (kafkaUrl string, terminateFunction func(context.Context) error, err error) {
-	return StartKafkaWithEnv(ctx, map[string]string{})
-}
-
-func StartKafkaWithEnv(ctx context.Context, env map[string]string) (kafkaUrl string, terminateFunction func(context.Context) error, err error) {
-	var containerHost string
-	var kafkaPortExpositionHost string
-	if val, exists := os.LookupEnv("HOST_IP"); exists {
-		containerHost = val
-		kafkaPortExpositionHost = val
-	} else {
-		containerHost = GetOutboundIP()
-		kafkaPortExpositionHost = "0.0.0.0"
-	}
-
-	hostPortInt, err := GetFreePort(kafkaPortExpositionHost)
-	if err != nil {
-		return "", nil, err
-	}
-
-	logrus.Infof("containerHost: %s, kafkaPortExpositionHost: %s ", containerHost, kafkaPortExpositionHost)
-
-	hostPort := fmt.Sprintf("%d", hostPortInt)
-	kafkaExternalAdvertisedListener := containerHost + ":" + hostPort
-	logrus.Debugf("kafkaExternalAdvertisedListener: %s", kafkaExternalAdvertisedListener)
-
-	// hostPort:containerPort/protocol
-	portExpositionString := kafkaPortExpositionHost + ":" + hostPort + ":9092"
-	logrus.Debugf("kafka port exposition: %s", portExpositionString)
-
-	identifier := strings.ToLower(uuid.New().String())
-
-	composeFilePaths := []string{"docker-compose.yml"}
-
-	compose := testcontainers.NewLocalDockerCompose(composeFilePaths, identifier)
-
-	envWithDefaults := map[string]string{
-		"KAFKA_PORT_EXPOSITION":              portExpositionString,
-		"KAFKA_EXTERNAL_ADVERTISED_LISTENER": kafkaExternalAdvertisedListener,
-	}
-
-	for k,v := range env {
-		envWithDefaults[k] = v
-	}
-
-	execError := compose.
-		WithCommand([]string{"up", "-d"}).
-		WithEnv(envWithDefaults).
-		Invoke()
-	if execError.Error != nil {
-		logrus.Errorf("%s failed with\nstdout:\n%v\nstderr:\n%v", execError.Command, execError.Stdout, execError.Stderr)
-		return "", nil, fmt.Errorf("could not run compose file: %v - %w", composeFilePaths, execError.Error)
-	}
-
-	logrus.Infof("kafka started at %s", kafkaExternalAdvertisedListener)
-
-	return kafkaExternalAdvertisedListener, func(ctx context.Context) error {
-		execError := compose.Down()
-		if execError.Error != nil {
-			logrus.Errorf("%s failed with\nstdout:\n%v\nstderr:\n%v", execError.Command, execError.Stdout, execError.Stderr)
-			return fmt.Errorf("'docker-compose down' failed for compose file: %v - %w", composeFilePaths, err)
-		}
-		return nil
-	}, nil
-}
-
-// inspired by https://github.com/phayes/freeport
-func GetFreePort(host string) (int, error) {
-	hostWithPort := fmt.Sprintf("%s:0", host)
-	addr, err := net.ResolveTCPAddr("tcp", hostWithPort)
-	if err != nil {
-		fmt.Printf("could not resolve %s: %s", hostWithPort, err)
-		return GetFreePortFallback(host)
-	}
-
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		fmt.Printf("could not listen to %s: %s", hostWithPort, err)
-		return GetFreePortFallback(host)
-	}
-	defer l.Close()
-	return l.Addr().(*net.TCPAddr).Port, nil
-}
-
-func GetFreePortFallback(host string) (int, error) {
-	port := getRandomPort()
-	for isOpen(host, port) {
-		port = getRandomPort()
-	}
-	return port, nil
-}
-
-func getRandomPort() int {
-	rand.Seed(time.Now().UnixNano())
-	min := 49152
-	max := 65535
-	return rand.Intn(max - min) + min
-}
-
-func isOpen(host string, port int) bool {
-	return raw_connect(host, strconv.Itoa(port)) == nil
-}
-
-// see https://stackoverflow.com/questions/56336168/golang-check-tcp-port-open
-func raw_connect(host string, port string) error {
-	timeout := time.Second
-	hostAndPort := net.JoinHostPort(host, port)
-	conn, err := net.DialTimeout("tcp", hostAndPort, timeout)
-	if err != nil {
-		fmt.Printf("could not connect to %s: %s - (which is good, because we are looking for an unused port ;-) )", hostAndPort, err)
-		return err
-	}
-	defer conn.Close()
-	return nil
-}
-
-// Get preferred outbound ip of this machine
-// see https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
-func GetOutboundIP() string {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	return localAddr.IP.String()
 }
